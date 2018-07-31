a072a939f652c07582455f37c44ba0802a41549d
Warren Leyes <warren.leyes@gmail.com>
2018-07-31 16:26:04 -0700

    started to move project cards to JS file

--- data/projects.js
+++ data/projects.js
@@ -1,83 +1,142 @@
-[
+let project = [
   {
-    "title": "Personal Profile Page",
-    "description": "Customize a personal online profile by adding graphics, customizing text, and improving the look of a web page using Cascading Style Sheets, the formatting language of the web. Share your finished project on GitHub, a website used by teams of programmers to share and work on programming code.",
-    "skills": ['html', 'css'],
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-01",
-    "preview": "https://leyes.ca/treehouse-fewd-project-01"
+    title: "Personal Profile Page",
+    image: "img/project1.PNG",
+    alt: "Project 1 screenshot",
+    description:
+      "Customize a personal online profile by adding graphics, customizing text, and improving the look of a web page using Cascading Style Sheets, the formatting language of the web. Share your finished project on GitHub, a website used by teams of programmers to share and work on programming code.",
+    player_ID: "1",
+    video_title: "Project 1",
+    video_MP4: "media/project1.mp4",
+    video_OGG: "media/project1.ogg",
+    skills: ["html", "css"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-01",
+    preview: "https://leyes.ca/treehouse-fewd-project-01"
   },
   {
-    "title": "Responsive Layout",
-    "description": "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
-    "skills": ['html', 'css'],
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-02",
-    "preview": "https://leyes.ca/treehouse-fewd-project-02"
+    title: "Responsive Layout",
+    image: "img/project2.PNG",
+    alt: "Project 2 screenshot",
+    description:
+      "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
+    player_ID: "2",
+    video_title: "Project 2",
+    video_MP4: "media/project2.mp4",
+    video_OGG: "media/project2.ogg",
+    skills: ["html", "css"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-02",
+    preview: "https://leyes.ca/treehouse-fewd-project-02"
   },
   {
-    "title": "Online Registration Form",
-    "description": "Web forms appear everywhere online: forms allow users to order books, sign up for web sites, and post to Facebook. This project challenges you to build a responsive, mobile-first registration form using a variety of HTML form elements.",
-    "skills": ['html', 'css', 'JavaScript'],
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-03",
-    "preview": "https://leyes.ca/treehouse-fewd-project-03"
+    title: "Online Registration Form",
+    image: "img/project3.PNG",
+    alt: "Project 3 screenshot",
+    description:
+      "Web forms appear everywhere online: forms allow users to order books, sign up for web sites, and post to Facebook. This project challenges you to build a responsive, mobile-first registration form using a variety of HTML form elements.",
+    player_ID: "1",
+    video_title: "Project 3",
+    video_MP4: "media/project3.mp4",
+    video_OGG: "media/project3.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-03",
+    preview: "https://leyes.ca/treehouse-fewd-project-03"
   },
   {
-    "title": "Interactive Photo Gallery",
-    "description": "Interactive image galleries are a common feature of many websites, from photo sites to e-commerce applications. Use HTML, CSS and the popular programming language JavaScript to create an interactive, searchable gallery of photos.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-04",
-    "preview": "https://leyes.ca/treehouse-fewd-project-04"
+    title: "Interactive Photo Gallery",
+    image: "img/project4.PNG",
+    alt: "Project 4 screenshot",
+    description:
+      "Interactive image galleries are a common feature of many websites, from photo sites to e-commerce applications. Use HTML, CSS and the popular programming language JavaScript to create an interactive, searchable gallery of photos.",
+    player_ID: "4",
+    video_title: "Project 4",
+    video_MP4: "media/project4.mp4",
+    video_OGG: "media/project4.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-04",
+    preview: "https://leyes.ca/treehouse-fewd-project-04"
   },
   {
-    "title": "Build a Web Style Guide",
-    "description": "Sass is an important tool in a modern Front End Web Developer’s toolbox. It's used by many developers to make styling web pages with CSS easier and faster. Create a style guide that can act as your own personal and custom Bootstrap that you can drop into any of your projects to speed up styling, layout and development in general.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-05",
-    "preview": "https://leyes.ca/treehouse-fewd-project-05"
+    title: "Build a Web Style Guide",
+    image: "img/project5.PNG",
+    alt: "Project 5 screenshot",
+    description:
+      "Sass is an important tool in a modern Front End Web Developer’s toolbox. It's used by many developers to make styling web pages with CSS easier and faster. Create a style guide that can act as your own personal and custom Bootstrap that you can drop into any of your projects to speed up styling, layout and development in general.",
+    player_ID: "5",
+    video_title: "Project 5",
+    video_MP4: "media/project5.mp4",
+    video_OGG: "media/project5.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-05",
+    preview: "https://leyes.ca/treehouse-fewd-project-05"
   },
   {
-    "title": "Interactive Video Player",
-    "description": "Build an HTML5 video player using JavaScript and the HTML5 Video API. Video, and the interface used to control the video, is increasingly important for many different kinds of web applications, including web site landing pages, blogs, podcasts, product marketing and training.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-06",
-    "preview": "https://leyes.ca/treehouse-fewd-project-06"
+    title: "Interactive Video Player",
+    image: "img/project6.PNG",
+    alt: "Project 6 screenshot",
+    description:
+      "Build an HTML5 video player using JavaScript and the HTML5 Video API. Video, and the interface used to control the video, is increasingly important for many different kinds of web applications, including web site landing pages, blogs, podcasts, product marketing and training.",
+    player_ID: "6",
+    video_title: "Project 6",
+    video_MP4: "media/project6.mp4",
+    video_OGG: "media/project6.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-06",
+    preview: "https://leyes.ca/treehouse-fewd-project-06"
   },
   {
-    "title": "Build a Game Show App",
-    "description": "In this project, you'll create a browser version of “Wheel of Success”, a word guessing game. You’ll use Javascript to come up with a random phrase that players will try to guess by entering different letters into the program.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-07",
-    "preview": "https://leyes.ca/treehouse-fewd-project-07"
+    title: "Build a Game Show App",
+    image: "img/project7.PNG",
+    alt: "Project 7 screenshot",
+    description:
+      "In this project, you'll create a browser version of “Wheel of Success”, a word guessing game. You’ll use Javascript to come up with a random phrase that players will try to guess by entering different letters into the program.",
+    player_ID: "7",
+    video_title: "Project 7",
+    video_MP4: "media/project7.mp4",
+    video_OGG: "media/project7.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-07",
+    preview: "https://leyes.ca/treehouse-fewd-project-07"
   },
   {
-    "title": "SVG Animations",
-    "description": "Master Scaleable Vector Graphics. SVG graphics are quickly becoming one of the most widely used graphic formats on the web. Modernize the user interface of a web page by adding, styling and optimizing SVG graphics.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-08",
-    "preview": "https://leyes.ca/treehouse-fewd-project-08"
+    title: "SVG Animations",
+    image: "img/project8.PNG",
+    alt: "Project 8 screenshot",
+    description:
+      "Master Scaleable Vector Graphics. SVG graphics are quickly becoming one of the most widely used graphic formats on the web. Modernize the user interface of a web page by adding, styling and optimizing SVG graphics.",
+    player_ID: "8",
+    video_title: "Project 8",
+    video_MP4: "media/project8.mp4",
+    video_OGG: "media/project8.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-08",
+    preview: "https://leyes.ca/treehouse-fewd-project-08"
   },
   {
-    "title": "Web App Dashboard",
-    "description": "Build an interactive dashboard for a web application using advanced web techniques including SVG graphics and JavaScript programming. The project involves creating tables, charts, graphics and other user interface components in a manner that promotes interactivity and usability.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-09",
-    "preview": "https://leyes.ca/treehouse-fewd-project-09"
+    title: "Web App Dashboard",
+    image: "img/project9.PNG",
+    alt: "Project 9 screenshot",
+    description:
+      "Build an interactive dashboard for a web application using advanced web techniques including SVG graphics and JavaScript programming. The project involves creating tables, charts, graphics and other user interface components in a manner that promotes interactivity and usability.",
+    player_ID: "9",
+    video_title: "Project 9",
+    video_MP4: "media/project9.mp4",
+    video_OGG: "media/project9.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-09",
+    preview: "https://leyes.ca/treehouse-fewd-project-09"
   },
   {
-    "title": "Use a Public API to Create a Front End for an Employee Directory",
-    "description": "Many sites — Twitter, Facebook, IMDB, and Wikipedia to name a few — offer a vast sea of data that you can access and display on your own web pages. Using JavaScript, you’ll create an employee directory by communicating with a third-party API (Application Programming Interface).",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-10",
-    "preview": "https://leyes.ca/treehouse-fewd-project-10"
+    title: "Use a Public API to Create a Front End for an Employee Directory",
+    image: "img/project10.PNG",
+    alt: "Project 10 screenshot",
+    description:
+      "Many sites — Twitter, Facebook, IMDB, and Wikipedia to name a few — offer a vast sea of data that you can access and display on your own web pages. Using JavaScript, you’ll create an employee directory by communicating with a third-party API (Application Programming Interface).",
+    player_ID: "10",
+    video_title: "Project 10",
+    video_MP4: "media/project10.mp4",
+    video_OGG: "media/project10.ogg",
+    skills: ["html", "css", "JavaScript"],
+    repository: "https://github.com/WLeyes/treehouse-fewd-project-10",
+    preview: "https://leyes.ca/treehouse-fewd-project-10"
   },
-  {
-    "title": "Weather App",
-    "description": "A simple weather app using wunderground.com API",
-    "repository": "https://github.com/WLeyes/weatherAPI",
-    "preview": "https://leyes.ca/weatherAPI"
-  },
-  {
-    "title": "Responsive Layout",
-    "description": "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-02",
-    "preview": "https://leyes.ca/treehouse-fewd-project-02"
-  },
-  {
-    "title": "Responsive Layout",
-    "description": "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
-    "repository": "https://github.com/WLeyes/treehouse-fewd-project-02",
-    "preview": "https://leyes.ca/treehouse-fewd-project-02"
-  },
-]
+];
--- js/app.js
+++ js/app.js
@@ -32,10 +32,66 @@ const DataCtrl = (() => {
 const UICtrl = (() => {
   messageArray = [];
   const UISelectors = {
-    recentActivity: "#recent-activity"
+    recentActivity: "#recent-activity",
+    cards: ".cards"
   };
 
   return {
+    cards: card => {
+      for (let i = 0; i < project.length; i++) {
+        let title = project[i].title;
+        let image = project[i].image;
+        let alt = project[i].alt;
+        let description = project[i].description;
+        let player_id = project[i].player_ID;
+        let video_title = project[i].video_title;
+        let video_mp4 = project[i].video_MP4;
+        let video_ogg = project[i].video_OGG;
+        let skills = project[i].skills;
+        let repository = project[i].repository;
+        let preview = project[i].preview;
+        let card = `
+        <div class="card grid__col--12">
+        <div class="card--front">
+          <h3>${title}</h3>
+          <img src="${image}" alt="${alt}">
+          <h3>Summary:</h3>
+          <p>${description}</p>
+        </div>
+        <div class="card--back">
+          <video controls id="player${player_id}" class="player" title="${video_title}">
+            <source src="${video_mp4}" type="video/mp4">
+            <source src="${video_ogg}" type="video/ogg">
+            <!-- Flash fallback -->
+            <object type="application/x-shockwave-flash" data="flash-player.swf?videoUrl=${video_mp4}">
+              <param name="movie" value="flash-player.swf?videoUrl=${video_mp4}" />
+              <param name="allowfullscreen" value="true" />
+              <param name="wmode" value="transparent" />
+              <param name="flashvars" value="controlbar=over&amp;image=img/Team-Treehouse-Review.jpg&amp;file=flash-player.swf?videoUrl=videoUrl=${video_mp4}"
+              />
+              <p>"No video playback possible, please download the video from the link below"</p>
+            </object>
+            <!-- Offer download -->
+            <a href="${video_mp4}">Download MP4</a>
+          </video>
+          
+          <div class="btn--group grid__row">
+            <a href="${repository}" target="_blank" class="btn--default theme__colors">
+              <img src="img/github.svg" alt="Code svg" class="icons--github">view code
+            </a>
+            <a href="${preview}" target="_blank" class="btn--info theme__colors">
+              <img src="img/eye.svg" alt="Code svg" class="icons--preview">preview
+            </a>
+          </div>
+        </div>
+      </div>
+        `;
+        let cardElement = document.querySelector(UISelectors.cards);
+        console.log(cardElement);
+
+        cardElement.innerHTML += card;
+      }
+    },
     recentActivity: data => {
       let array = [];
       for (let i = 1; i < data.results.length; i++) {
@@ -51,7 +107,6 @@ const UICtrl = (() => {
             <p>${randomReview()}</p>
           </div>
         </div>        
-        
         `;
         let reviewer = document.querySelector(UISelectors.recentActivity);
         let output = document.createElement("div");
@@ -75,11 +130,15 @@ const App = ((UICtrl, DataCtrl, ChartCtrl) => {
   // Get UI selectors
   const UISelectors = UICtrl.getSelectors();
 
-  const loadEventListeners = () => {};
+  const loadEventListeners = () => { };
   return {
     init: () => {
       console.log("Initializing App ...");
       DataCtrl.getRandomUser();
+      UICtrl.cards();
+      // console.log(project[0]); // Test that projects are being imported
+      console.log(` Number of projects: ${project.length}`);
+
       loadEventListeners();
     }
   };
