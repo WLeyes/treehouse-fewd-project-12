4eb9924452badd1d3f2905f46f8f071855cb6a77
Warren Leyes <2774431+WLeyes@users.noreply.github.com>
2018-07-30 18:28:24 -0700

    added basic mobile layout and started on styling

--- data/data.js
+++ data/data.js
@@ -10,13 +10,17 @@ let review = [
   "Warren takes great pride in his work",
   "Did I leave the stove on?"
 ];
-let stars = `<img class="star" src="img/star.svg" alt="star"/>`;
+let stars = `
+<svg class="stars" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/></svg>
+`;
 function randomReview() {
   let random_review = review[Math.floor(Math.random() * review.length)];
   let random_stars = stars;
   for (let i = 0; i < Math.floor(Math.random() * (5 - 2) + 2); i++) {
     random_stars += stars;
   }
-  let output = `${random_review} "${random_stars}"`;
+  let output = `
+    ${random_review}<br>
+    Stars: "<span class="stars">${random_stars}</span>"`;
   return output;
 }
---
+++ data/projects.js
@@ -0,0 +1,83 @@
+[
+  {
+    "title": "Personal Profile Page",
+    "description": "Customize a personal online profile by adding graphics, customizing text, and improving the look of a web page using Cascading Style Sheets, the formatting language of the web. Share your finished project on GitHub, a website used by teams of programmers to share and work on programming code.",
+    "skills": ['html', 'css'],
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-01",
+    "preview": "https://leyes.ca/treehouse-fewd-project-01"
+  },
+  {
+    "title": "Responsive Layout",
+    "description": "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
+    "skills": ['html', 'css'],
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-02",
+    "preview": "https://leyes.ca/treehouse-fewd-project-02"
+  },
+  {
+    "title": "Online Registration Form",
+    "description": "Web forms appear everywhere online: forms allow users to order books, sign up for web sites, and post to Facebook. This project challenges you to build a responsive, mobile-first registration form using a variety of HTML form elements.",
+    "skills": ['html', 'css', 'JavaScript'],
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-03",
+    "preview": "https://leyes.ca/treehouse-fewd-project-03"
+  },
+  {
+    "title": "Interactive Photo Gallery",
+    "description": "Interactive image galleries are a common feature of many websites, from photo sites to e-commerce applications. Use HTML, CSS and the popular programming language JavaScript to create an interactive, searchable gallery of photos.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-04",
+    "preview": "https://leyes.ca/treehouse-fewd-project-04"
+  },
+  {
+    "title": "Build a Web Style Guide",
+    "description": "Sass is an important tool in a modern Front End Web Developer’s toolbox. It's used by many developers to make styling web pages with CSS easier and faster. Create a style guide that can act as your own personal and custom Bootstrap that you can drop into any of your projects to speed up styling, layout and development in general.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-05",
+    "preview": "https://leyes.ca/treehouse-fewd-project-05"
+  },
+  {
+    "title": "Interactive Video Player",
+    "description": "Build an HTML5 video player using JavaScript and the HTML5 Video API. Video, and the interface used to control the video, is increasingly important for many different kinds of web applications, including web site landing pages, blogs, podcasts, product marketing and training.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-06",
+    "preview": "https://leyes.ca/treehouse-fewd-project-06"
+  },
+  {
+    "title": "Build a Game Show App",
+    "description": "In this project, you'll create a browser version of “Wheel of Success”, a word guessing game. You’ll use Javascript to come up with a random phrase that players will try to guess by entering different letters into the program.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-07",
+    "preview": "https://leyes.ca/treehouse-fewd-project-07"
+  },
+  {
+    "title": "SVG Animations",
+    "description": "Master Scaleable Vector Graphics. SVG graphics are quickly becoming one of the most widely used graphic formats on the web. Modernize the user interface of a web page by adding, styling and optimizing SVG graphics.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-08",
+    "preview": "https://leyes.ca/treehouse-fewd-project-08"
+  },
+  {
+    "title": "Web App Dashboard",
+    "description": "Build an interactive dashboard for a web application using advanced web techniques including SVG graphics and JavaScript programming. The project involves creating tables, charts, graphics and other user interface components in a manner that promotes interactivity and usability.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-09",
+    "preview": "https://leyes.ca/treehouse-fewd-project-09"
+  },
+  {
+    "title": "Use a Public API to Create a Front End for an Employee Directory",
+    "description": "Many sites — Twitter, Facebook, IMDB, and Wikipedia to name a few — offer a vast sea of data that you can access and display on your own web pages. Using JavaScript, you’ll create an employee directory by communicating with a third-party API (Application Programming Interface).",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-10",
+    "preview": "https://leyes.ca/treehouse-fewd-project-10"
+  },
+  {
+    "title": "Weather App",
+    "description": "A simple weather app using wunderground.com API",
+    "repository": "https://github.com/WLeyes/weatherAPI",
+    "preview": "https://leyes.ca/weatherAPI"
+  },
+  {
+    "title": "Responsive Layout",
+    "description": "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-02",
+    "preview": "https://leyes.ca/treehouse-fewd-project-02"
+  },
+  {
+    "title": "Responsive Layout",
+    "description": "Designing web pages that look and function well on multiple screen sizes is an essential skill for a web developer. Using HTML, CSS, and responsive design, you'll create a mobile-first web page with a layout that adjusts to fit mobile phones, tablets and desktop displays.",
+    "repository": "https://github.com/WLeyes/treehouse-fewd-project-02",
+    "preview": "https://leyes.ca/treehouse-fewd-project-02"
+  },
+]
